3b7f1ad791bb8bb77310d8a417737ae876401426
Hasibul294 <hasibulhasan294@gmail.com>
2021-11-13 18:54:57 +0600

    complete

---
+++ index.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ua7v2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("scootyBazar");
+    const productCollection = database.collection("products");
+    const orderCollection = database.collection("order");
+    const usersCollection = database.collection("users");
+    const reviewCollection = database.collection("review");
+
+    //Get API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //Dynamic API
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
+
+    //Delete API
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //User Get API
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    //User Post API
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    //User PUt API
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const option = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, option);
+      res.json(result);
+    });
+
+    //Make admin API
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    //Review Post API
+    app.post("/review", async (req, res) => {
+      const item = req.body;
+      const result = await reviewCollection.insertOne(item);
+      res.json(result);
+    });
+
+    //Product Post API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+
+    //Booking Post API
+    app.post("/product/booking", async (req, res) => {
+      const booking = req.body;
+      booking.status = "pending";
+      const result = await orderCollection.insertOne(booking);
+      res.json(result);
+    });
+
+    //User Order Get API
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { email: id };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //Orders Get API
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
+    });
+
+    //Update API
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: "confirm",
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      res.json(result);
+    });
+
+    //Delete API
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World! I am Scooty Bazar");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
